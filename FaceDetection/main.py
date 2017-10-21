15b3141a136f8196dfbe0fa8f25447a12f4cbc23
Roman Bond <romanjbond@gmail.com>
2017-10-21 14:57:58 -0400

    Moved work space to active repo

--- FaceDetection/main.py
+++ FaceDetection/main.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+#refs: https://stackoverflow.com/questions/34871294/full-body-detection-and-tracking-using-opencvpython-2-7
+#refs: https://pythonprogramming.net/haar-cascade-face-eye-detection-python-opencv-tutorial/
+
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 # https://github.com/opencv/opencv/tree/master/data/haarcascades
 
@@ -24,28 +27,10 @@ def draw_detections(img, rects, thickness = 1):
         pad_w, pad_h = int(0.15*w), int(0.05*h)
         cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), thickness)
 
-
-# if __name__ == '__main__':
-#
-#     hog = cv2.HOGDescriptor()
-#     hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-#     cap=cv2.VideoCapture(0)
-#     while True:
-#         _,frame=cap.read()
-#         found,w=hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.05)
-#         draw_detections(frame,found)
-#         cv2.imshow('frame',frame)
-#         ch = 0xFF & cv2.waitKey(1)
-#         if ch == 27:
-#             break
-
-
-
 while 1:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
@@ -53,11 +38,9 @@ while 1:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-
 
     found, w = hog.detectMultiScale(img, winStride=(8, 8), padding=(32, 32), scale=1.05)
     draw_detections(img, found)
